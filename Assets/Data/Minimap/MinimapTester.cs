86e36fd75ded49724856e72dead9855afa9cab72
KaustavDG <kaustavdasgupta0@gmail.com>
2026-05-17 22:12:19 +0530

    adjusted to show map in center

--- Assets/Data/Minimap/MinimapTester.cs
+++ Assets/Data/Minimap/MinimapTester.cs
@@ -4,13 +4,14 @@ using UnityEngine.UI;
 public class MinimapTester : MonoBehaviour
 {
     [SerializeField]
+    private RectTransform minimap;
+
+    [SerializeField]
     private RectTransform dot;
 
-    // README example coordinate
     private float worldX = -301.45f;
     private float worldZ = -355.55f;
 
-    // Ambrose Valley config
     private float originX = -370f;
     private float originZ = -473f;
     private float scale = 900f;
@@ -19,10 +20,12 @@ public class MinimapTester : MonoBehaviour
     {
         float u = (worldX - originX) / scale;
         float v = (worldZ - originZ) / scale;
-        float pixelX = u * 1024f;
-        float pixelY = (1f - v) * 1024f;
+        float pixelX = u * minimap.rect.width;
+        float pixelY = (1f - v) * minimap.rect.height;
+        float localX = pixelX - minimap.rect.width * 0.5f;
+        float localY = (minimap.rect.height * 0.5f) - pixelY;
 
-        dot.anchoredPosition = new Vector2(pixelX, -pixelY);
-        Debug.Log($"Pixel: {pixelX}, {pixelY}");
+        dot.anchoredPosition =new Vector2(localX, localY);
+        Debug.Log($"Dot Position: {dot.anchoredPosition}");
     }
 }
